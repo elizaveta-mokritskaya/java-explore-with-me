91f2f5a8732d36a9bf9dd426404ace7dd57bd081
My Name <aro.volturi0411@yandex.ru>
2024-04-15 11:16:30 +0300

    main_svc логи и зависимость

--- main-service/src/main/java/ru/practicum/event/PublicEventController.java
+++ main-service/src/main/java/ru/practicum/event/PublicEventController.java
@@ -35,15 +35,17 @@ public class PublicEventController {
                                                 @PositiveOrZero @RequestParam(defaultValue = "0") Integer from,
                                                 @Positive @RequestParam(defaultValue = "10") Integer size,
                                                 HttpServletRequest request) {
-        log.debug("!!!!!!!!!!!получаем ивенты по фильтру!!!!!!!!!!!!!!!!!!!!!");
+
         List<EventShortDto> events = service.getEventByFilter(text, categories, paid,
                 rangeStart,
                 rangeEnd,
                 onlyAvailable, sort,
                 from / size, size);
-        log.debug("!!!!!!!форматируем и добавляем HitDto!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        client.addHit(new HitDto("ewm-service", request.getRequestURI(), request.getRemoteAddr(),
-                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
+        log.debug("!!!!!!!!!!!получаем ивенты по фильтру!!!!!!!!!!!!!!!!!!!!!", events);
+        HitDto hitDto = new HitDto("ewm-service", request.getRequestURI(), request.getRemoteAddr(),
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        client.addHit(hitDto);
+        log.debug("!!!!!!!форматируем и добавляем HitDto!!!!!!!!!!!!!!!!!!!!!!!!!!", hitDto);
         return events;
     }
 
